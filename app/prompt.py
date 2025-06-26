16f2f499fc32d5ede4c3af4d6444dc23ca84e6ed
Hanchung Lee <lee.hanchung@gmail.com>
2025-06-26 04:21:54 +0000

    feat: upgrade to streamlit

--- app/prompt.py
+++ app/prompt.py
@@ -1,6 +1,20 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+WELCOME_MESSAGE = """\
+Welcome to Introduction to LLM App Development Sample PDF QA Application!
+To get started:
+1. Upload a PDF or text file
+2. Ask any question about the file!
+"""
+
 ##############################################################################
 # Exercise 1:
-# Please navigate and find the Langchain prompt templates used for 
-# RetrievalQAWithSources chain and how it is initialized.
+# Please create a ChatPromptTemplate with message history.
+#
+# This documentation will help you
+# https://python.langchain.com/api_reference/core/prompts/langchain_core.prompts.chat.ChatPromptTemplate.html
 ##############################################################################
-from langchain.prompts import PromptTemplate
+PROMPT = ChatPromptTemplate.from_messages(
+    ...
+)
+##############################################################################
